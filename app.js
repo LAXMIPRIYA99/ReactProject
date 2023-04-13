ea28622e234895f6dc73af2e834c752c608cec4f
LAXMIPRIYA99 <62532553+LAXMIPRIYA99@users.noreply.github.com>
2023-04-13 16:12:49 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import  ReactDOM  from "react-dom/client";
 
-const head = React.createElement(
-    "h1", {} , "Hello world using React"
+const head = <h1> Hello world using React </h1>;//single line jsx
+const head1 = (
+    <h2 className="head1">
+        H2 element
+    </h2>
 )
-const head1= [React.createElement(
-    "h1", {id:"head1"} , "h1 element"
-), React.createElement(
-    "h2" , {id:"head2"}, "h2 element"
-)]
-const head2 = React.createElement(
-    "div", {} , React.createElement(
-      "h3", {}, "I am in h3 inside div"
-    )
-)
+const head2 =(  //multi line jsx
+    <div>
+        <h3 className="head2">
+            I am in h3             
+        </h3>
+    </div>
+);
+
+
+const Title = () => {  return <h1>Title component</h1> }
+const FunComponent = () =>(
+    <div>
+        {head1} //acessing js code using "{}"
+        {head2}
+        {Title()}
+        <Title/>
+        <h1>Example functional component</h1>
+    </div>
+);
 const root = ReactDOM.createRoot(
     document.getElementById("root")
 )
@@ -23,6 +35,10 @@ const root1 = ReactDOM.createRoot(
 const root2= ReactDOM.createRoot(
     document.getElementById("root2")
 )
+const root3= ReactDOM.createRoot(
+    document.getElementById("root3")
+)
 root.render(head);
 root1.render(head1);
 root2.render(head2);
+root3.render(<FunComponent/>);
